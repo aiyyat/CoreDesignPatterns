65eeb1625900f2ff13e47bdadcd27f87f80f871d
achuth <achuth.i@gmail.com>
2015-12-24 07:58:39 +0800

    rectifying a bug introduced initially when changing from immutable class to mutable one.

--- Patterns/src/org/technicalyorker/dp/behavioural/strategy/sort/strategies/ReverseSelectionSortStrategy.java
+++ Patterns/src/org/technicalyorker/dp/behavioural/strategy/sort/strategies/ReverseSelectionSortStrategy.java
@@ -3,15 +3,14 @@ package org.technicalyorker.dp.behavioural.strategy.sort.strategies;
 public class ReverseSelectionSortStrategy extends SortStrategy {
 	@Override
 	public void sort(int[] numoriginal) {
-		int[] num = numoriginal.clone();
 		int i, j, a;
-		for (i = num.length - 1; i > 0; i--) {
+		for (i = numoriginal.length - 1; i > 0; i--) {
 			a = 0;
 			for (j = 1; j <= i; j++) {
-				if (num[j] < num[a])
+				if (numoriginal[j] < numoriginal[a])
 					a = j;
 			}
-			swap(num, i, a);
+			swap(numoriginal, i, a);
 
 		}
 	}
